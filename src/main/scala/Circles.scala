c169bef1088dcd3dd1cd10b3208359a51ecc9d80
Stefan Ackermann <acki.ch@gmail.com>
2016-01-17 12:45:08 +0100

    convert to doubles for positions and velocities. withinBounds is now slow (~25 ms for 1000 circles)

--- src/main/scala/Circles.scala
+++ src/main/scala/Circles.scala
@@ -20,11 +20,14 @@ class Circles {
           random.nextInt(width),
           random.nextInt(height),
           random.nextDouble() * 10,
-          random.nextInt(3) - 1,
-          random.nextInt(3) - 1
+          (random.nextDouble() - 0.5) * 5,
+          (random.nextDouble() - 0.5) * 5
         )
     }
   }
+
+  addCircles(2000, 1400, 1000)
+
   def checkForCollisions() = {
     for {
       (circle1, index) <- circles.zipWithIndex
@@ -35,30 +38,36 @@ class Circles {
   }
 
   def updateCircles(width: Int, height: Int): Vector[Circle] = {
+    StopWatch.start("Update Position")
     circles.foreach(_.updatePosition())
+    StopWatch.start("Check bounds")
     circles = circles.filter(_.withinBounds(width, height))
+    StopWatch.start("After check bounds")
     circles
   }
 
   def tick(width: Int, height: Int): Unit = {
     tick += 1
     if (tick % 10 == 0) {
-      addCircles(width, height, 10)
+      addCircles(width, height, 1000 - circles.size)
     }
     updateCircles(width, height)
   }
 
 }
 
-class Circle(var x: Int,
-             var y: Int,
+class Circle(var x: Double,
+             var y: Double,
              var radius: Double,
-             var xVelocity: Int = 1,
-             var yVelocity: Int = 0,
+             var xVelocity: Double = 1,
+             var yVelocity: Double = 0,
              var color: Color = Color.black) {
 
+  def xInt = Math.round(x)
+  def yInt = Math.round(y)
+
   def withinBounds(width: Int, height: Int): Boolean = {
-    (0 to width).contains(x) && (0 to height).contains(y)
+    (0 to width).contains(xInt) && (0 to height).contains(yInt)
   }
 
   def collidesWith(circle2: Circle) = {
@@ -78,17 +87,16 @@ class Circle(var x: Int,
 
   def drawTo(g: Graphics2D): Unit = {
     g.setPaint(color)
-    val double: Ellipse2D.Double = asEllipsis
-    g.fill(double)
+    g.fill(asEllipsis)
   }
 
   def asEllipsis: Ellipse2D.Double = {
-    val double = new Ellipse2D.Double(x, y, radius, radius)
-    double
+    new Ellipse2D.Double(x, y, radius, radius)
   }
 
   override def toString: String =
-    s"$x $y $radius"
+    s"$x/$y ($radius) $xVelocity/$yVelocity"
+
 }
 
 case class Collision(cir1: Circle, cir2: Circle)
